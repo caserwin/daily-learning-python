aecfdb7ab41938647b33d0452e3b29ad9582d919
caserwin <zustxueyiding@163.com>
2018-07-30 15:55:19 +0800

    daily upfate

---
+++ core/util/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/30 上午9:17
+# @Author  : yidxue
--- core/util/file_util.py
+++ core/util/file_util.py
@@ -1,7 +1,7 @@
-# coding:utf-8
-from os.path import join, getsize
-import shutil
+# -*- coding: utf-8 -*-
 import os
+import shutil
+from os.path import join, getsize
 
 """
 http://andylin02.iteye.com/blog/1071448
@@ -34,8 +34,9 @@ class FileUtil(object):
         循环遍历,得到一个文件夹第一层下的文件路径
         """
         import os
+        path = path.decode("utf-8").encode('utf-8')
         file_name_list = os.listdir(path)
-        return [path+os.sep+file_name for file_name in file_name_list]
+        return [path + os.sep + file_name for file_name in file_name_list]
 
     @staticmethod
     def get_all_file_name(path):
@@ -54,7 +55,6 @@ class FileUtil(object):
         :param method: 'a'表示默认为追加方式, 'wb'表示覆盖或者创建文件写入
         :param context:
         """
-        file_path = unicode(file_path, "utf8")
         with open(file_path, method) as fo:
             fo.write(context)
         # 关闭打开的文件
@@ -66,11 +66,20 @@ class FileUtil(object):
         读数据
         """
         line_list = []
-        file_path = unicode(file_path, "utf8")
-        with open(file_path, "r") as in_file:
+        with open(file_path, mode="r") as in_file:
             for line in in_file:
                 line_list.append(line.strip())
         return line_list
 
+
 if __name__ == '__main__':
-    print(FileUtil.get_all_file_path('/Users/haizhi/xyd'))
+    print(FileUtil.get_dir_size('/Users/cisco/workspace/mygit/daily-learning-python/core/util/test'))
+    print('=' * 40)
+    print('\n'.join(FileUtil.get_all_file_path('/Users/cisco/workspace/mygit/daily-learning-python/core/util/test')))
+    print('=' * 40)
+    print('\n'.join(FileUtil.get_all_file_name('/Users/cisco/workspace/mygit/daily-learning-python/core/util/test')))
+    print('=' * 40)
+    line_list = FileUtil.read_file('/Users/cisco/workspace/mygit/daily-learning-python/core/util/test/tmp.txt')
+    print('\n'.join(line_list))
+    print('=' * 40)
+    FileUtil.write_file('/Users/cisco/workspace/mygit/daily-learning-python/core/util/test/tmp2.txt', "你好!!")
